4eb07fc68a4c21b4b36de4fefdfa904f2ed719be
chrisdvkta <chrisdevkota77@gmail.com>
2025-02-01 23:08:09 +0545

    quicksort

---
+++ QuickSort.py
@@ -0,0 +1,18 @@
+arr = [42 ,23 ,36, 11 ,65, 58 ,94, 74, 99, 87]
+
+
+def QuickSort(arr, l , r): 
+    for i in range(len(arr)):
+        while (l<r): 
+            pass
+
+
+
+
+
+
+
+
+
+
+QuickSort(arr, 0, len(arr)-1)
