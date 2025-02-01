c39a8b81e348aabd2159a4e07a33dce33ab50bff
chrisdvkta <chrisdevkota77@gmail.com>
2025-02-01 11:04:08 +0545

    minmax

--- minMax.py
+++ minMax.py
@@ -2,20 +2,26 @@ arr = [7 ,10, 14, 9 ,11 ,32, 52, 74 ,6]
 
 
 def MinMax(arr, l,r): 
-    m = int((l+r)/2)
     if (l==r): 
-        max = min = arr[l]
+        minVal = maxVal = arr[l]
     elif (l==r-1):
-        if (arr[l]>arr[r]): 
-            max = arr[l]
-            min = arr[r]
-        else:
-            max = arr[r]
-            min = arr[l]    
+         minVal = min(arr[l],arr[r])
+         maxVal = max(arr[l], arr[r])
+    else: 
+        mid = int((l+r)/2)
+        min1,max1= MinMax(arr,l,mid)
+        min2,max2= MinMax(arr,mid+1,r)
+        minVal = min(min1,min2)
+        maxVal = max(max1,max2)
+    return (minVal,maxVal)
 
 
 
-    left = MinMax(arr,l,m)
-    right = MinMax(arr,m+1,r)
+print(MinMax(arr,0,len(arr)-1))
+
+
+        
+
+
 
 
